523890a938a16181b694f8064bdf449f32780598
Paulo Preto <paulopreto.dev@gmail.com>
2024-04-21 21:44:20 +0100

    scaffold ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,29 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/b1215519-4152-41a6-b65f-41f485ccf051-i906yh.png",
+  "https://utfs.io/f/314978aa-7011-486d-9fab-bab54f31b1fb-i906wt.png",
+  "https://utfs.io/f/39a6e40d-7f2b-4f26-8cd7-1dc92b6f561b-i906aa.png",
+  "https://utfs.io/f/d5993950-a606-4306-833c-24e9a741cfa2-i9069b.png",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages, ...mockImages].map(
+          (image) => (
+            <div key={image.id} className="w-48">
+              <img src={image.url} />
+            </div>
+          ),
+        )}
+      </div>
       Hello (gallery in progress)
     </main>
   );
