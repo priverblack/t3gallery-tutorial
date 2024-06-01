e7c7c4c4a4862d76d77fefcb5e81fc5618183468
Paulo Preto <paulopreto.dev@gmail.com>
2024-06-01 15:56:49 +0100

    start implementing parallel routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Image from "next/image";
-import { db } from "~/server/db";
+import Link from "next/link";
 import { getMyImages } from "~/server/queries";
 
 // this makes the page dynamic so that every refresh fetches the page again
@@ -13,13 +13,15 @@ async function Images() {
     <div className="flex flex-wrap justify-center gap-4">
       {images.map((image) => (
         <div key={image.id} className="flex h-48 w-48 flex-col">
-          <Image
-            src={image.url}
-            style={{ objectFit: "contain" }}
-            width={192}
-            height={192}
-            alt={image.name}
-          />
+          <Link href={`/img/${image.id}`}>
+            <Image
+              src={image.url}
+              style={{ objectFit: "contain" }}
+              width={192}
+              height={192}
+              alt={image.name}
+            />
+          </Link>
         </div>
       ))}
     </div>
