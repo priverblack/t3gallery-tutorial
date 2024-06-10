8b1c7f38f73c5690869e7b0f78dcd616badcc2d7
Paulo Preto <paulopreto.dev@gmail.com>
2024-06-10 18:07:12 +0100

    add own upload button

--- src/app/img/[id]/page.tsx
+++ src/app/img/[id]/page.tsx
@@ -1,4 +1,4 @@
-import FullPageImageView from "~/components/full-image-page";
+import FullPageImageView from "~/common/full-image-page";
 
 export default function ImagePage({
   params: { id: imageId },
--- src/common/full-image-page.tsx
+++ src/common/full-image-page.tsx
@@ -7,12 +7,12 @@ export default async function FullPageImageView(props: { id: number }) {
   const uploaderInfo = await clerkClient.users.getUser(image.userId);
 
   return (
-    <div className="flex h-full w-full min-w-0 ">
-      <div className="flex flex-shrink items-center justify-center">
+    <div className="flex h-full w-screen min-w-0 items-center justify-center text-white">
+      <div className="flex-shrink flex-grow">
         <img src={image.url} className="flex-shrink object-contain" />
       </div>
 
-      <div className="flex w-56 flex-shrink-0 flex-col border-l border-white">
+      <div className="flex h-full w-56 flex-shrink-0 flex-col border-l">
         <div className="border-b p-2 text-center text-lg">{image.name}</div>
 
         <div className="flex flex-col p-2">
