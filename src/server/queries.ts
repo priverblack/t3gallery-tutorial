19cdd3ea774bcc3da4fed4ab961005b789e5e215
Paulo Preto <paulopreto.dev@gmail.com>
2024-06-30 20:37:06 +0100

    add analytics for the delete event

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -3,8 +3,8 @@ import { db } from "./db";
 import { auth } from "@clerk/nextjs/server";
 import { images } from "./db/schema";
 import { and, eq } from "drizzle-orm";
-import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import analyticsServerClient from "./analytics";
 
 export async function getMyImages() {
   const user = auth();
@@ -42,6 +42,14 @@ export async function deleteImage(id: number) {
   await db
     .delete(images)
     .where(and(eq(images.id, id), eq(images.userId, user.userId)));
+
+  analyticsServerClient.capture({
+    distinctId: user.userId,
+    event: "delete image",
+    properties: {
+      imageId: id,
+    },
+  });
 
   redirect("/");
 }
