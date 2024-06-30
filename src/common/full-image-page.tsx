23062148a2850d0311682278f3eb738d943c2248
Paulo Preto <paulopreto.dev@gmail.com>
2024-06-30 20:25:03 +0100

    add delete image feature

--- src/common/full-image-page.tsx
+++ src/common/full-image-page.tsx
@@ -1,8 +1,12 @@
 import { clerkClient } from "@clerk/nextjs/server";
-import { getImage } from "~/server/queries";
+import { Button } from "~/components/ui/button";
+import { deleteImage, getImage } from "~/server/queries";
 
 export default async function FullPageImageView(props: { id: number }) {
-  const image = await getImage(props.id);
+  const idAsNumber = Number(props.id);
+  if (Number.isNaN(idAsNumber)) throw new Error("Invalid photo id");
+
+  const image = await getImage(idAsNumber);
 
   const uploaderInfo = await clerkClient.users.getUser(image.userId);
 
@@ -23,6 +27,20 @@ export default async function FullPageImageView(props: { id: number }) {
         <div className="flex flex-col p-2">
           <span>Created on:</span>
           <span>{new Date(image.createdAt).toLocaleDateString()}</span>
+        </div>
+
+        <div className="flex flex-col p-2">
+          <form
+            action={async () => {
+              "use server";
+
+              await deleteImage(idAsNumber);
+            }}
+          >
+            <Button type="submit" variant={"destructive"}>
+              Delete
+            </Button>
+          </form>
         </div>
       </div>
     </div>
